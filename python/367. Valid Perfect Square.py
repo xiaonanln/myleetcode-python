1d6547fab005b406ad98f4472a6a1b380bf68f46
seis <xiaonanln@gmail.com>
2017-09-06 00:50:06 +0800

    learning algorithms...

--- python/367. Valid Perfect Square.py
+++ python/367. Valid Perfect Square.py
@@ -1,23 +1,28 @@
 class Solution(object):
-    def isPerfectSquare(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-        if num in (1, 4): return True 
-        if num <= 4: return False 
-        # now, num > 4
-        i, j = 1, num
-        while i <= j:
-            print i, j
-            m = (i+j) // 2
-            mm = m*m
-            if mm == num:
-                return True 
-            elif mm < num:
-                i = m+1
-            else: # mm > num
-                j = m-1
-                
-        return False 
-            
+	def isPerfectSquare(self, num):
+		"""
+		:type num: int
+		:rtype: bool
+		"""
+		l, r = 1, num
+
+		while l <= r:
+
+			m = (l + r) // 2
+			mm = m * m
+			# print l, r, m, mm, '->',
+			if num < mm:
+				r = m - 1
+			elif num > mm:
+				l = m + 1
+			else:
+				return True
+
+			# print l, r, m, mm
+
+		return False
+
+# print Solution().isPerfectSquare(1)
+print Solution().isPerfectSquare(2)
+# print Solution().isPerfectSquare(4)
+# print Solution().isPerfectSquare(16)
